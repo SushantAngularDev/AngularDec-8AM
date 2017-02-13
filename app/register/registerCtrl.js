15e289944f1f2d936bc88b7c821cf31f2f39f740
Sushant <Sushant>
2017-02-13 10:52:56 +0530

    added file

--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -1,6 +1,5 @@
 (function(){
-    
-    function registerCtrl(lookupSvc){
+    function registerCtrlFn(){
         var vm = this;
         vm.user={
             firstName:""
@@ -8,47 +7,7 @@
         vm.register=function(){
             console.log(vm.user);
         }
-       // vm.countries = lookupSvc.getCountries();
-        
-        lookupSvc.getCountries()
-        .then( function(res){
-            console.log(res);
-        })
-        .catch(function(err){
-            console.log(err);
-        })
-        //vm.selectedCountry = lookupSvc.defaultCountry();
-       /* angular.forEach( vm.countries, function(item){
-           if(item.code=="IN"){
-               //vm.selectedCountry=item;
-           }
-        })*/
-        //get country list from json file
-        lookupSvc.getCountriesFromJson()
-        .then( function(res){
-            console.log(res);
-            vm.countries= res;
-        })
-        .catch(function(err){
-            console.log(err);
-        })
-        
-    }
+    };
     angular.module("register")
-    .controller("registerCtrl",["lookupService",registerCtrl]);
-    
+    .controller("registerCtrl",[registerCtrlFn]);
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
