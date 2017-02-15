f13d4308c98c5f8ee90dd6cd00cb46b05f0d11af
Sushant <Sushant>
2017-02-15 23:25:12 +0530

    filter code added

--- app/product/productCtrl.js
+++ app/product/productCtrl.js
@@ -1,4 +1,4 @@
-(function(){
+/*(function(){
     
     function productCtrlFn(productSvc){
 		var vm=this;
@@ -15,3 +15,51 @@
     angular.module("product")
     .controller("productsCtrl",["productService",productCtrlFn])
 })();
+*/
+
+
+
+(function(){
+    
+    function productsCtrlFn(productsSvc){
+        var vm = this;
+       productsSvc.getProductFromJson()
+        .then( function(res){
+            console.log(res);
+            vm.products= res.data.products;
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+        vm.minimumProducts=10;
+        vm.showMore = function(){
+          vm.minimumProducts+=10;  
+        }; 
+        vm.orderByCrietria="Price";
+        vm.orderByPrice = function(){
+            if( vm.orderByCrietria=="Price"){
+                vm.orderByCrietria="-Price";
+            }
+            else{
+                 vm.orderByCrietria="Price";
+            }
+        }
+    }
+    angular.module("product")
+    .controller("productsCtrl",["productService",productsCtrlFn]);
+    
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
